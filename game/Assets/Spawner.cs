1fd62eede1d505ae5aa84b8542e29254e8ce6162
garywhi <50818672+garywhi@users.noreply.github.com>
2020-02-02 15:01:28 -1000

    Prefabs and tool spawns

---
+++ game/Assets/Spawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] tools;
+    public Transform spawnPoint;
+
+    private int rand;
+
+    public float startTimeBtwSpawns;
+    private float timeBtwSpawns;
+
+    private void Start()
+    {
+        timeBtwSpawns = startTimeBtwSpawns;
+    }
+
+    private void Update()
+    {
+        if (timeBtwSpawns <= 0)
+        {
+            Instantiate(tools[0], spawnPoint.transform.position, Quaternion.identity);
+            timeBtwSpawns = startTimeBtwSpawns;
+        }
+        else
+        {
+            timeBtwSpawns -= Time.deltaTime;
+        }
+    }
+}
